1bbdf60b74a8daa49fd6a5c0db35cb95a8f2aa79
신정아 <jeonga@khu.ac.kr>
2022-06-27 22:26:51 +0900

    init project

---
+++ src/main/java/group/redisforcache/RedisForCacheApplication.java
@@ -0,0 +1,13 @@
+package group.redisforcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisForCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisForCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/redisforcache/RedisForCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package group.redisforcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisForCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
