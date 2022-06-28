bef92aa445c6b08fdb6d1838a2c2c3bc6d4e37f8
신정아 <shinjeonga@twentyoz.kr>
2022-06-28 16:34:11 +0900

    TO-DO : Serialization for cache

--- src/main/java/group/redisforcache/feature/model/Person.java
+++ src/main/java/group/redisforcache/feature/model/Person.java
@@ -1,6 +1,7 @@
 package group.redisforcache.feature.model;
 
 import group.redisforcache.feature.model.common.RegMod;
+import java.io.Serializable;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -23,7 +24,16 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString(callSuper = true)
 //@EqualsAndHashCode(callSuper = true)
-public class Person{ //extends RegMod
+public class Person { //extends RegMod  //extends RegMod implements Serializable
+
+  //public static final long serialVersionUID = 42L;
+  /**
+   * serialVersionUID를 지정해주지 않으면 정말 큰일납니다
+   *
+   * 왜냐하면, 다른 시스템끼리 통신해서 직렬화/역직렬화를 진행하는데
+   *
+   * 같은 객체라도 serialVersion이 다르면 역직렬화시 다른 객체로 판단해버리기 때문입니다!
+   */
 
   @Id
   //@Id 어노테이션이 붙은 필드가 Redis Key 값이 되며 null 로 세팅하면 랜덤값이 설정됨 //keyspace 와 합쳐져서 레디스에 저장된 최종 키 값은 keyspace:id가 됨
